c536adb3024bf8374c437c695145b851336317a4
Juanes <juan.echavarria@techandsolve.com>
2018-08-08 16:02:01 -0500

    SpringBoot

--- README.md
+++
@@ -1,1 +0,0 @@
-# tureceta-backend-java
--- src/main/java/com/tureceta/backend/BackendApplication.java
+++ src/main/java/com/tureceta/backend/BackendApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TuRecetaBackendApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TuRecetaBackendApplication.class, args);
-    }
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
 }
